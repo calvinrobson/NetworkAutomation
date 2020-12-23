c643e5a508425a4e9115dcb803087eae5e191bd8
Calvin Robson <calvin.robson94@gmail.com>
2020-12-23 20:08:48 +0000

    "Addition of Manufacturer"

--- NetworkAutomation/Collect MAC Address Table/AssetRegisterNetMikowithoutCommandParameters.py
+++ NetworkAutomation/Collect MAC Address Table/AssetRegisterNetMikowithoutCommandParameters.py
@@ -17,6 +17,13 @@ SHOW_VERSION_REGEXP = [
     re.compile(r'(?P<Version>ID\s[A-Z0-9]+)', re.M)
     ]
 
+SHOW_MANUFACTURER_REGEXP = [
+    re.compile(r'(?P<Version>ID\s[A-Z0-9]+)', re.M)
+    ]
+
+SHOW_IMAGE_REGEXP = [
+    re.compile(r'(?P<Version>ID\s[A-Z0-9]+)', re.M)
+    ]
 
 def read_yaml(path = 'devices.yml'):
     with open(path) as f:
@@ -39,17 +46,24 @@ def get_serial_number(cli_output):
         result_dictionary.update(regexp.search(cli_output).groupdict())
     return result_dictionary
 
+def get_manufacturer_number(manufac_output):
+    manufacturer_dict = dict()
+    for regexp in SHOW_MANUFACTURER_REGEXP
+        manufacturer_dict.update(regexp.search(manufac_output).groupdict())
+    return manufacturer_dict
+
 def netmiko_connection(parameters):
     for device in parameters:
         hostname = device.pop('hostname')
-        connect = netmiko.ConnectHandler(**device)
-        version = get_serial_number(connect.send_command('show version'))
-        return version
-        with open ('test.csv', 'w', newline='\r\n') as a:
+        connection = netmiko.ConnectHandler(**device)
+        version = get_serial_number(connection.send_command('show version'))
+        manufacture = get_manufacturer_number(cinnection.send_command('show version'))
+        print(hostname, version)
+        with open ('test.csv', 'w', newline='') as a:
             write = csv.writer(a)
             columns = write.writerow(['Hostname', 'Serial Number', 'Version and System Image'])
-            write.writerow(version)
-        connect.disconnect()
+            write.writerow([version,hostname,''])
+        connection.disconnect()
 
 def main():
     logins = read_yaml()
