3c50b7ec1bedd84c9cca28f86c6862c193b622c1
calvinrobson <calvin.robson94@gmail.com>
2020-12-29 16:55:25 +0000

    test 3

--- NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/main.py
+++ NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/main.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 from nornir import InitNornir
-from nornir.plugins.functions.text import print_result
-from nornir.plugins.functions.text import netmiko_send_command
+from nornir_utils.plugins.functions import print_result
+from nornir_netmiko import netmiko_send_command
 
 def main():
     with InitNornir("config.yaml") as nr:
