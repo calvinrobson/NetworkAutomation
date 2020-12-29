21bd17036fb8307838c2f8ac91ba494b0c0d297d
Calvin Robson <calvin.robson94@gmail.com>
2020-12-29 16:21:00 +0000

    "first test"

--- NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/main.py
+++ NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/main.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 from nornir import InitNornir
-from nornir.plugins.tasks import
+from nornir.plugins.functions.text import print_result
+from nornir.plugins.functions.text import netmiko_send_command
 
 def main()
     with InitNornir("config.yaml") as nr
+        result = nr.run(task=netmiko_send_command, command_string="show cdp neighbors")
+        print_result(result)
 
 if __name__ == '__main__':
     main()
