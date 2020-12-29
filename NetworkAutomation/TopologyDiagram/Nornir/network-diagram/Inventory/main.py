48d129dc29b8f640f8f7ee8c442638d5937753a1
Calvin Robson <calvin.robson94@gmail.com>
2020-12-29 15:55:00 +0000

    "addition"

--- NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/main.py
+++ NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 
 from nornir import InitNornir
+from nornir.plugins.tasks import 
 
 def main()
-    with 
+    with InitNornir("config.yaml") as nr
 
 if __name__ == '__main__':
     main()
