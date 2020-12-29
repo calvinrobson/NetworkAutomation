24fc12612810a7b9212ed3ea1cff481b3605b603
Calvin Robson <calvin.robson94@gmail.com>
2020-12-29 16:28:53 +0000

    "Fix for the indentation for nr"

--- NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/main.py
+++ NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/main.py
@@ -5,7 +5,7 @@ from nornir.plugins.functions.text import print_result
 from nornir.plugins.functions.text import netmiko_send_command
 
 def main():
-    with InitNornir("config.yaml") as nr
+    with InitNornir("config.yaml") as nr:
         result = nr.run(task=netmiko_send_command, command_string="show cdp neighbors")
         print_result(result)
 
