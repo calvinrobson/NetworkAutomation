858337f789a2f18832dae7686bc49270a051f48c
Calvin Robson <calvin.robson94@gmail.com>
2020-12-31 11:37:28 +0000

    "results and tasks"

--- NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/test2.py
+++ NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/test2.py
@@ -6,6 +6,7 @@ from nornir_netmiko import netmiko_send_command
 
 def get_interfaces(task):
     r = task.run(netmiko_send_command, command_string="show cdp neighbor", use_textfsm=True)
+    task.host["int"] = r.result
 
 def main():
     with InitNornir("config.yaml") as nr:
