d87fcdecd9eb41bb4cbed5684c85e5026d29f61a
Calvin Robson <calvin.robson94@gmail.com>
2021-01-02 15:42:54 +0000

    "interface has been changed"

--- NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/interface.py
+++ NetworkAutomation/TopologyDiagram/Nornir/network-diagram/Inventory/interface.py
@@ -1,4 +1,7 @@
-class interfaces:
+from typing import Optional
+
+
+class Interface:
     def __init__(self, name: str, device_name: Optional[str] = None) -> None:
         self.name = name
         self.device_name = self.device_name
@@ -12,3 +15,11 @@ class interfaces:
     
     def __str__(self) -> str:
         return f"{self.device_name} {self.name}"
+    
+    def __lt__(self) -> bool:
+        return (self.device_name, self.name) < (other.device_name, other.name)
+
+
+
+Link(Interface('gig1', 'R1'), Interface('gig1', 'R2'))
+Link(Interface('gig1', 'R2'), Interface('gig1', 'R1'))
